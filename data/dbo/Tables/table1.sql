e2e21a2471d9d61f2113e7b2a0aa18185f6abd3b
krzysztof.gil <krzysztof.gil@techdata.com>
2017-11-03 14:11:48 +0000

    Added 2 tables into data and staging databases

---
+++ data/dbo/Tables/table1.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	Field1 VARCHAR(30)
+)
---
+++ staging/dbo/Tables/stg_table1.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[stg_table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	Field1 VARCHAR(100)
+)
